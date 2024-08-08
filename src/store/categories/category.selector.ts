d7358a448e7529e9617e6c0cd5b8e297b2260b4a
Raven470 <rat0917@gmail.com>
2024-08-08 11:35:07 +0800

    change categories into typescript type

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,7 +1,9 @@
 import { createSelector } from "reselect";
+import { CategoriesState } from "./category.reducer";
+import { CategoryMap } from "./category.types";
 
 // initial selector
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = (state): CategoriesState => state.categories;
 
 // memoize selector
 export const selectCategories = createSelector(
@@ -11,24 +13,15 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
+  (categories): CategoryMap =>
     categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
   [selectCategoryReducer],
   (categories) => categories.isLoading
 );
-
-/* 
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
- */
